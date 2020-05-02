6c70936c63f64d8c07a05f9bd6e4eb13521e6484
Jordan Noe <jordanvinnoe@gmail.com>
2020-05-02 13:33:39 -0400

    fixed button and label displaying problem. Good to ship!

--- Develop/script.js
+++ Develop/script.js
@@ -7,39 +7,43 @@ var hrRound = moment().hours();
 
 
     for (i = 9; i < 18; i++){//create 9 new divs
-        // var newDiv = $("<div>").addClass("row");
+       
         if (i > 12){//with labels 1:00 through 5:00
             var label = $("<label>").text(i - 12 + ":00");
-        }
+            var newDiv = $("<div>").addClass("input-group row");
+            label.addClass("col-1");
+        } 
         else {//with labels 9:00 through 12:00
             var label = $("<label>").text(i + ":00");
+            label.addClass("col-1");
+            var newDiv = $("<div>").addClass("input-group row");
         }
         //if local storage at i exists at the index i, retrieve it and assign it to the value of the textarea
         if (localStorage.getItem(i)){
             var textArea = $("<textarea>").attr("id", i).val(localStorage.getItem(i));
-            textArea.addClass("form-control");
+            textArea.addClass("form-control time-block col-10");
         }
         else{
             var textArea = $("<textarea>").attr("id", i);
-            textArea.addClass("form-control");
+            textArea.addClass("form-control time-block");
         }
         
         //if hour block is in the past, grey background
-        if ( $("#" + i) < hrRound){
-            $(this).addClass("past");
+        if (i < hrRound){
+            textArea.addClass("past");
         }
         //if hour block current, green bg
         else if (i == hrRound){
-            $(this).addClass("present");
+            textArea.addClass("present");
         }
         //if hour block future, red bg
         else {
-            $(this).addClass("future");
+            textArea.addClass("future");
         }
         
-        var button = $("<button>").addClass("submit saveBtn").text("submit");
-        $(".container").append(textArea);
-        (textArea).append(button);
+        var button = $("<button>").addClass("submit saveBtn col-1").text("submit");
+        $(".container").append(newDiv);
+        $(newDiv).append(label, textArea, button);
     }
     
     
